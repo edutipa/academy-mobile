c6f3d210dbd5a7124dfe5462ef053d5a4939b2e9
fikriks <fikriks130@gmail.com>
2024-06-09 11:43:09 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/edutipa/academy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.edutipa.academy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static const String baseUrl = 'https://academy.edutipa.com/api/';
+
+  static const double appVersion = 1.0;
+  static const String oneSignalAppID = 'ae61d771-2305-4f06-ae0a-c3bc4587e65c';
+}
